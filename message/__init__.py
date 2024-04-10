8a16be9611eb8e1c33b70bdce225761ffd44cab3
DamyanBG <d.dimitrov.1988@abv.bg>
2024-04-10 15:26:15 +0300

    add actionName

--- message/__init__.py
+++ message/__init__.py
@@ -5,6 +5,7 @@ import azure.functions as func
 
 def main(data, actions: func.Out[str]) -> None:
     actions.set(json.dumps({
+        'actionName': 'sendToAll',
         'data': '[SYSTEM] ack.',
         'dataType': 'text'
     }))
