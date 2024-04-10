a9eeeb88df18f7368585163d8f4b703ed05c98c5
DamyanBG <d.dimitrov.1988@abv.bg>
2024-04-10 15:26:58 +0300

    add logging

--- message/__init__.py
+++ message/__init__.py
@@ -1,9 +1,12 @@
 import json
+import logging
 
 import azure.functions as func
 
 
 def main(data, actions: func.Out[str]) -> None:
+    logging.info(data)
+    logging.info(actions)
     actions.set(json.dumps({
         'actionName': 'sendToAll',
         'data': '[SYSTEM] ack.',
