27aed886a9b4d894d9b81acc5e02ebfc585e193e
DamyanBG <d.dimitrov.1988@abv.bg>
2024-04-11 08:47:25 +0300

    formatting

--- message/__init__.py
+++ message/__init__.py
@@ -1,14 +1,16 @@
 import json
-import logging
 
 import azure.functions as func
 
 
-def main(data, actions: func.Out[str]) -> None:
-    logging.info(data)
-    logging.info(actions)
-    actions.set(json.dumps({
-        'actionName': 'sendToAll',
-        'data': '[SYSTEM] ack.',
-        'dataType': 'text'
-    }))
+def main(request, actions: func.Out[str]) -> None:
+    req_json = json.loads(request)
+    actions.set(
+        json.dumps(
+            {
+                "actionName": "sendToAll",
+                "data": f'[{req_json["connectionContext"]["userId"]}] {req_json["data"]}',
+                "dataType": req_json["dataType"],
+            }
+        )
+    )
--- negotiate/__init__.py
+++ negotiate/__init__.py
@@ -1,4 +1,5 @@
 import azure.functions as func
 
+
 def main(req: func.HttpRequest, connection) -> func.HttpResponse:
     return func.HttpResponse(connection)
